297dcfd1f2a3e3fd5b196603bebe7001fbbbace8
zpx2012 <zhupengxiong@yeah.net>
2018-03-07 04:32:04 -0800

    server ip address

--- test case/hclient.c
+++ test case/hclient.c
@@ -20,7 +20,7 @@ int main(int argc , char *argv[])
     }
     puts("Socket created");
      
-    server.sin_addr.s_addr = inet_addr("127.0.0.1");
+    server.sin_addr.s_addr = inet_addr("169.235.31.230");
     server.sin_family = AF_INET;
     server.sin_port = htons( 8888 );
  
