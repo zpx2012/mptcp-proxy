23671c6f3693f5f356201c2f63c97e6a63610bf8
zpx2012 <zhupengxiong@yeah.net>
2018-05-01 10:05:17 -0700

    log

--- mptcp_proxy.c
+++ mptcp_proxy.c
@@ -1030,7 +1030,7 @@ void eval_packet(uint32_t id, size_t hook, unsigned char *buf, u_int16_t len) {
 	//new
 	char s_ip_rem[20], tmp_msg[30];
 	sprintIPaddr(s_ip_rem, packd.ft.ip_rem);	
-	snprintf(msg_buf,MAX_MSG_LENGTH, "eval_packet: ip_rem %s, prt_loc %d,",s_ip_rem,packd.ft.prt_loc);		
+	snprintf(msg_buf,MAX_MSG_LENGTH, "\neval_packet: ip_rem %s, prt_loc %d,",s_ip_rem,packd.ft.prt_loc);		
 	if(packd.sfl){
 		if(packd.sfl == packd.sess->slav_subflow){
 			packd.is_master = 0;
