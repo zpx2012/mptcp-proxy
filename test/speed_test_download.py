f6fd37f07bad6ebfdd6c0d863fb50b94ad0ab76f
JimZyq <jimmyzyq95@gmail.com>
2018-05-29 02:12:06 -0700

    print speed every 10s

--- test/speed_test_download.py
+++ test/speed_test_download.py
@@ -1,16 +1,20 @@
 import io,pycurl,sys,os,time,datetime,traceback
 
 download_last = 0
-last_time = ""
+last_time = int(round(time.time()))
 file_name = ""
 
 def progress(download_t, download_d, upload_t, upload_d):
-    new_time = datetime.datetime.now()
-    speed = ((download_d-download_last)/(new_time-last_time)/1024)
-    global last_time = new_time
-    global download_last = download_d
-    with open(file_name,"a")
-        f.writelines(localtime + "\t  %10.3f \n" %(speed/1024)) 
+    global last_time
+    global download_last
+    new_time = int(round(time.time()))
+    if((new_time-last_time)>10):
+        speed = ((download_d-download_last)/(new_time - last_time))
+        last_time = new_time
+        download_last = download_d
+        localtime = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))
+        with open(file_name,"a") as f:
+            f.writelines(localtime + "\t  %10.3fk/s \n" %(speed/1024)) 
 
 def test_download_socks(test_url,output_file):
     with open('/dev/null','wb') as test_f:
@@ -18,7 +22,7 @@ def test_download_socks(test_url,output_file):
             c = pycurl.Curl()
             c.setopt(pycurl.WRITEDATA,test_f)
             c.setopt(pycurl.NOPROGRESS,False)
-            c.setopt(pycurl.XFERINFORFUNCTION,progress)
+            c.setopt(pycurl.XFERINFOFUNCTION,progress)
             c.setopt(pycurl.URL,test_url)
             c.setopt(pycurl.PROXY,'socks5h://127.0.0.1')
             c.setopt(pycurl.PROXYPORT,1080)
@@ -38,7 +42,7 @@ def test_download_socks(test_url,output_file):
             print ('total time: %10.3f s' %(total_time))
             print ('localtime : ' + localtime)
             with open(output_file,"a") as f:
-                f.writelines(localtime + "\t  %10.3f  %10.3f\n" %(speed/1024,total_time)) # \t speed(k/s) \t total_time(ms)
+                f.writelines(localtime + "\t  %10.3fk/s  %10.3fs Download Finished\n" %(speed/1024,total_time)) # \t speed(k/s) \t total_time(s)
             c.close()
 
 def test_download(test_url,output_file):
@@ -48,7 +52,7 @@ def test_download(test_url,output_file):
                 c.setopt(pycurl.WRITEDATA,test_f)
                 c.setopt(pycurl.ENCODING,'gzip')
                 c.setopt(pycurl.NOPROGRESS,False)
-                c.setopt(pycurl.XFERINFORFUNCTION,progress)
+                c.setopt(pycurl.XFERINFOFUNCTION,progress)
                 c.setopt(pycurl.URL,test_url)
                 c.setopt(pycurl.MAXREDIRS,5)
                 c.perform()
@@ -66,7 +70,7 @@ def test_download(test_url,output_file):
                 print ('total time: %10.3f s' %(total_time))
                 print ('localtime : ' + localtime)
                 with open(output_file,"a") as f:
-                    f.writelines(localtime + "\t %10.3f  %10.3f\n" %(speed/1024,total_time)) # speed(k/s) \t total_time(ms)
+                    f.writelines(localtime + "\t  %10.3fk/s  %10.3fs Download Finished\n" %(speed/1024,total_time)) # speed(k/s) \t total_time(s)
                 c.close()
 
 if __name__ == '__main__':
@@ -81,7 +85,7 @@ if __name__ == '__main__':
             f.writelines("\n")
         while True:
             print ('Task : %d' %(num_tasks))
-            last_time = datetime.datetime.now()
+            last_time = int(round(time.time()))
             download_last = 0
             test_download(test_url,file_name)
             num_tasks = num_tasks +1
@@ -93,7 +97,7 @@ if __name__ == '__main__':
             f.writelines("\n")
         while True:
             print ('Task : %d' %(num_tasks))
-            last_time = datetime.datetime.now()
+            last_time = int(round(time.time()))
             download_last = 0
             test_download_socks(test_url,file_name)
             num_tasks = num_tasks +1
