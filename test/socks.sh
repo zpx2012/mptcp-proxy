eb928de446d6d8932ff1876e53397a0da3224e1d
JimZyq <jimmyzyq95@gmail.com>
2018-06-03 19:24:36 -0700

    key

--- test/socks.sh
+++ test/socks.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-chmod 400 ~/mptcp-proxy/test/mptcp_aws_1.pem
+chmod 400 ~/mptcp_aws_1.pem
 ssh -D 127.0.0.1:1080 -i "~/mptcp_aws_1.pem" ubuntu@ec2-52-33-71-249.us-west-2.compute.amazonaws.com
