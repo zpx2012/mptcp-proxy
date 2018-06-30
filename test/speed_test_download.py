a76a60bf859af654cbae77db26719acffaf51ca9
zpx2012 <zhupengxiong@yeah.net>
2018-06-30 08:55:36 +0800

    [test] add tc.sh; change to httpserver url

--- test/speed_test_download.py
+++ test/speed_test_download.py
@@ -65,7 +65,7 @@ if __name__ == '__main__':
         sys.exit(-1)
     option = sys.argv[1]   #0->vpn 1->socks
     os.system("mkdir %s" % results_dir_abs_path)
-    test_url = "http://mirror.enzu.com/ubuntu-releases/ubuntu-core/16/ubuntu-core-16-pi2.img.xz"
+    test_url = "http://172.31.21.225:8080/ubuntu-core-16-pi2.img.xz"
     start = datetime.datetime.now()
     sys_hostname = socket.gethostname()
     if(option == '0'):
@@ -86,6 +86,6 @@ if __name__ == '__main__':
             pycurl_regular(test_url)
         else:
             pycurl_socks(test_url)
-        time.sleep(10)
+#        time.sleep(10)
     
     
