049365614bdcc3db0bf8c2dbe3ed2a83fd8e93d4
zpx2012 <zhupengxiong@yeah.net>
2018-07-03 06:39:46 -0400

    [test] add stdout print

--- test/speed_test_download.py
+++ test/speed_test_download.py
@@ -5,6 +5,7 @@ download_last = 0
 last_time = int(round(time.time()))
 output_file_name = ""
 results_dir_abs_path = expanduser("~") + "/results"
+# zero_speed_counter = 0
 
 def call_back(download_t, download_d, upload_t, upload_d):
     global last_time
@@ -18,6 +19,7 @@ def call_back(download_t, download_d, upload_t, upload_d):
         localtime = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))
         with open(output_file_name,"a") as f:
             f.writelines(localtime + "\t  %10.3fk/s \n" %(speed/1024)) 
+            print(localtime + "\t  %10.3fk/s \n" %(speed/1024))
 
 def pycurl_socks(test_url):
     c = pycurl.Curl()
